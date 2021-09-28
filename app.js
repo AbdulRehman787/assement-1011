20b487272c6f489d1b012750c26dfa24c43ccb8b
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-09-28 11:14:52 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+alert("Hello, Mr bean.\n HOW R YOU?")
+
+
+var a = "Hello World";
+
+alert(a)
+
+var a =" A smarter way to learn JavaScript";
+document.write(a)
+
+var a =  16 ;
+alert(a)
+
+
+var a = "30.06.2021"
+document.write(a)
